694fd85893feb3b4e3aa997fd31687c258c27879
liam-strand <Liam.Strand@tufts.edu>
2022-04-13 13:34:55 -0400

    tooling

---
+++ src/init_state.py
@@ -0,0 +1,15 @@
+import tomli as tomllib
+
+class Init_State:
+    def __init__(self, config_filename: str) -> None:
+            with open(config_filename, "rb") as f:
+                config = tomllib.load(f)
+                self.intersections = config["intersections"]
+                self.roads         = config["roads"]
+
+    def __repr__(self):
+        return f"{{intersections: {self.intersections},\nroads: {self.roads}}}"
+
+if __name__ == "__main__":
+    test = Init_State("roads.toml")
+    print(test.intersections)
