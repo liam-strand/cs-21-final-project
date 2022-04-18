845eddf4170be1568c9c15925ecd516324ad534d
Benjamin J. Duchild <bduchi01@vm-hw04.eecs.tufts.edu>
2022-04-17 20:26:55 -0400

    multiplexers and stuff

--- src/car.erl
+++ src/car.erl
@@ -1,5 +1,5 @@
 -module(car).
--export([new/2, run/1]).
+-export([new/2, run/2]).
 
 -record(car, {speed, pos=0, stops}).
 
@@ -8,25 +8,45 @@ new(Speed, Stops) ->
     #car{speed=Speed, stops=Stops}.
 
 
+run(Mux, Car) ->
+    C = update(Car),
+    send_state(Mux, C),
+    receive
+        go    -> run(Mux, C);
+        stop  -> ok
+    end.
+
+%% send a car state to the multiplexer
+send_state(Mux, Car) ->
+    Progress = Car#car.pos,
+    Road = 
+        case Car#car.stops of
+            [From|[To|_]] -> {From, To};
+            _ -> finished
+        end,
+    Mux ! {Road, Progress}.
+
+
 %% Have a car drive from its start intersection to its destination,
 %% abiding by all the traffic laws.
 %% 
 %% Finish if at end of path.
-run(#car{stops=[ ]}) -> 
+update(#car{stops=[ ]}) -> 
     io:format("~w finished!~n", [self()]);
-run(#car{stops=[_]}) -> 
+update(#car{stops=[_]}) -> 
     io:format("~w finished!~n", [self()]);
-run(#car{stops=[_,_],pos=P}) when P >= 1.0 -> 
+update(#car{stops=[_,_],pos=P}) when P >= 1.0 -> 
     io:format("~w finished!~n", [self()]);
 
 %% wait at intersection when we reach it
-run(Car) when Car#car.pos >= 1.0 -> 
-    run(wait(Car));
+update(Car) when Car#car.pos >= 1.0 -> 
+    wait(Car),
+    Car;
 
 %% otherwise just go along road
-run(Car) -> 
+update(Car) -> 
     timer:sleep(500),
-    run(Car#car{pos = Car#car.pos + Car#car.speed}).
+    Car#car{pos = Car#car.pos + Car#car.speed}.
 
 
 
